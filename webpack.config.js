af77720681eea6404c4717a696ef8c0cf6d5d138
Levon Avetisyan <levon05@inbox.ru>
2020-05-12 10:15:51 +0300

    follow changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'CalendarContainer.jsx'),
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: `bundle.js`,
@@ -31,30 +31,17 @@ module.exports = {
       },
       {
         test: /\.(sa|sc|c)ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: process.env.NODE_ENV === 'development',
-            },
-          },
-          'css-loader',
-          'sass-loader'
-        ]
+        use: ['css-loader', 'sass-loader']
       }
     ]
+  },
+  externals: {
+    react: 'react'
   },
   devServer: {
     historyApiFallback: true,
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: `[name].css`,
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html'),
-      filename: 'index.html',
-    }),
     new MomentLocalesPlugin({
       localesToKeep: ['ru']
     })
